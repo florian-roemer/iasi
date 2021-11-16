applied correct scaling factor to eumetsat script

process_iasi.py
@@ -70,7 +70,7 @@ def create_mask(lat, land_frac, cloud_frac, domain):
 
     # create masks
     if 'tropics' in domain:
-        mask[:, :, :, 0] = np.logical_and(mask[:, :, :, 0], trop)
+         mask[:, :, :, 0] = np.logical_and(mask[:, :, :, 0], trop[:, :, :, 0])
     if 'clear-sky' in domain:
         mask[:, :, :, 0] = np.logical_and(mask[:, :, :, 0], clear_sky)
     if 'ocean-only' in domain:
@@ -79,7 +79,7 @@ def create_mask(lat, land_frac, cloud_frac, domain):
     return mask
 
 
-def masked_average(radiance, angle, mask):
+def masked_average(radiance, angle, mask, lat, scale):
     '''
     This functions averages the fluxes along track as well as over each
     scanning angle (4 pixels per scanning angle), apllying the three different
@@ -87,11 +87,9 @@ def masked_average(radiance, angle, mask):
     '''
     # apply masks and average over all scans and all pixels per scan
     rad = np.divide(np.sum(radiance * mask, axis=(0, 2)),
-                    np.sum(mask, axis=(0, 2)))
-    ang = np.divide(np.sum(angle * mask[:, :, :, 0], axis=(0, 2)),
-                    np.sum(mask[:, :, :, 0], axis=(0, 2)))
-
-    return rad, ang
+                    np.sum(mask * scale, axis=(0, 2)))
+    ang = np.divide(np.sum(angle * (mask * scale)[:, :, :, 0], axis=(0, 2)),
+                    np.sum((mask * scale)[:, :, :, 0], axis=(0, 2)))
 
 
 def average_symmetric_angles(ang, rad):
@@ -152,7 +150,7 @@ def calc_specflux(radcos, fullangle):
     return specflux
 
 
-def process_data(radiance, angle, mask, domain, orbit):
+def process_data(radiance, angle, mask, domain, orbit, lat, scale):
     '''
     Encapsulates processing steps applied to the different domains (masks).
     '''
@@ -160,7 +158,7 @@ def process_data(radiance, angle, mask, domain, orbit):
     nobs = np.count_nonzero(mask)
     frac = np.count_nonzero(mask)/mask.size
 
-    rad, ang = masked_average(radiance, angle, mask)
+    rad, ang = masked_average(radiance, angle, mask, lat, scale)
     meanangle, meanrad = average_symmetric_angles(ang, rad)
     fullangle, radcos = prepare_interpolation(meanangle, meanrad)
     radcos = interpolate(fullangle, radcos)
@@ -200,15 +198,27 @@ def main(FILE):
     land_frac = idata.GEUMAvhrr1BLandFrac[:, :, :]
 
     # ensure consistent dimensions
-    lat = lat.reshape(lat.shape[0], 30, 4)
+    lat = lat.reshape(lat.shape[0], 30, 4, 1)
     radiance = radiance.transpose((0, 2, 3, 1))
+    
+    # read factor correcting systematic oversampling of sub-polar latitudes
+    # using 180 bins from -90 to 90, they are centered at -89.5, -88.5 ... 89.5
+    factor = np.load('factor.npy')
+    index = np.round(lat + 89.5).astype('int')
+    weight = factor[index]
+    cos = np.cos(np.deg2rad(lat))
+    scale = cos*weight
+
+    # scale radiance with cosine of latitude and correct oversampling of
+    # sup-polar latitudes
+    radiance = radiance * scale
 
     for dom1 in ['global', 'tropics']:
         for dom2 in ['all-sky', 'clear-sky']:
             for dom3 in ['land+ocean', 'ocean-only']:
                 domain = f'{dom1}/{dom2}/{dom3}'
                 mask = create_mask(lat, land_frac, cloud_frac, domain)
-                process_data(radiance, angle, mask, domain, orbit)
+                process_data(radiance, angle, mask, domain, orbit, lat, scale)
 
 
 if __name__ == '__main__':
@@ -232,4 +242,4 @@ if __name__ == '__main__':
             pass
 
     end = time.process_time()
-    print(f'Your program needed {end - start} seconds ({(end-start)/60} minutes)')
+    print(f'Your program needed {(end-start)/60} minutes.')