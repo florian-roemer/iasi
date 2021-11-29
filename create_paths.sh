85b28a7fe9bde88b6d1c397d7c0434e30ed739ec
roemulus <roemerflo@gmail.com>
2021-11-29 14:51:10 +0100

    changed path for IASI data

--- create_paths.sh
+++ create_paths.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-data='/work/um0878/user_data/froemer/rare_mistral/data/IASI/test_new/test'
+data='/work/um0878/user_data/froemer/rare_mistral/data/IASI/final'
 
-domain1=( "global" "tropics")
-domain2=( "all-sky" "clear-sky")
-domain3=( "land+ocean" "ocean-only")
+domain1=( "global" "tropics" "extra" )
+domain2=( "all-sky" "clear-sky" )
+domain3=( "land+ocean" "ocean-only" ) 
 
 for dom1 in ${domain1[@]}
 do
@@ -16,10 +16,12 @@ for year in {2007..2021}
 do
 for month in {1..9}
 do
+echo $data/$dom1/$dom2/$dom3/$year/'0'$month
 mkdir -p $data/$dom1/$dom2/$dom3/$year/'0'$month
 done
 for month in {10..12}
 do
+echo $data/$dom1/$dom2/$dom3/$year/$month
 mkdir -p $data/$dom1/$dom2/$dom3/$year/$month
 done
 done
