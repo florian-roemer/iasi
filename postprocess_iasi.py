1fb984931c78eb8f5dff63aedfe453a625614fd0
roemulus <roemerflo@gmail.com>
2021-11-29 14:54:48 +0100

    used script to estimate error of extra/clear-sky by inferring it from monthly means of global/cs and trop/cs

--- postprocess_iasi.py
+++ postprocess_iasi.py
@@ -8,25 +8,25 @@ Created on Mon Feb  1 12:04:49 2021
 import sys
 import os
 import time
+import numpy as np
 
 
 def orbits_to_monthly_mean(domain, year, month):
-    import numpy as np
     from scipy import stats
     import pandas as pd
-
     import glob
-    import os
 
-    path = '../data/IASI/test_new/{}/{}/{}/'.format(domain, year, month)
+    path = '/work/um0878/user_data/froemer/rare_mistral/data/IASI/test_new/'\
+           'test/{}/{}/{}/'.format(domain, year, month)
 
-    fluxes_all = np.zeros((int(len(os.listdir(path))/2), 8461))
-    nobs_all = np.zeros((int(len(os.listdir(path))/2)))
+    fluxes_all = np.zeros((int(len(glob.glob(path + 'flux*', recursive=True))),
+                               8461))
+    area_all = np.zeros((int(len(glob.glob(path + 'area*', recursive=True)))))
     i, j = np.zeros(2, dtype=int)
 
     # Extract items containing numbers in name
     file = glob.glob(path + 'flux*', recursive=True)
-    file2 = glob.glob(path + 'nobs*', recursive=True)
+    file2 = glob.glob(path + 'area*', recursive=True)
 
     # Filter only files
     file = [f for f in file if os.path.isfile(f)]
@@ -37,44 +37,138 @@ def orbits_to_monthly_mean(domain, year, month):
         i += 1
 
     for filename in file2:
-        nobs_all[j] = np.load(filename)
+        area_all[j] = np.load(filename)
         j += 1
 
-    # sort out outliers (more than 3 sigma) before calcuating average
+    # sort out outliers (more than 5 sigma) before calcuating average
     # number of standard deviations as threshold
-    n = 3
+    # also sort out nans
+    n = 5
     fluxes = pd.DataFrame(fluxes_all)
-    nobs = pd.DataFrame(nobs_all)
-    fluxes2 = fluxes[(np.abs(stats.zscore(fluxes)) < n).all(axis=1)]
-    nobs = nobs[(np.abs(stats.zscore(fluxes)) < n).all(axis=1)]
+    area = pd.DataFrame(area_all)
+    fluxes2 = fluxes[(np.abs(stats.zscore(fluxes, nan_policy='omit')) < n)
+                     .all(axis=1)]
+    area = area[(np.abs(stats.zscore(fluxes, nan_policy='omit')) < n)
+                .all(axis=1)]
 
     fluxes = np.array(fluxes2)
-    nobs = np.array(nobs)[:, 0]
+    area = np.array(area)[:, 0]
 
     squared = fluxes**2
-    avg_squared = np.average(squared, weights=nobs, axis=0)
+    avg_squared = np.average(squared, weights=area, axis=0)
 
-    avg = np.average(fluxes, weights=nobs, axis=0)
+    avg = np.average(fluxes, weights=area, axis=0)
     std = np.std(fluxes, axis=0)
 
-    np.save(f'{path}/mean_flux', avg, allow_pickle=False, fix_imports=False)
-    np.save(f'{path}/mean_flux_squared', avg_squared, allow_pickle=False,
+    total_area = np.sum(area)
+
+    np.save(f'{path}/avg_flux.npy', avg, allow_pickle=False, fix_imports=False)
+    np.save(f'{path}/avg_flux_squared.npy', avg_squared, allow_pickle=False,
             fix_imports=False)
-    np.save(f'{path}/total_orbits', fluxes.shape[0],
+    np.save(f'{path}/total_area.npy', total_area,
             allow_pickle=False, fix_imports=False)
 
-    return fluxes, avg, std
+    return fluxes, avg, std, area
 
 
 if __name__ == '__main__':
     start = time.process_time()
-    os.chdir('/work/um0878/user_data/froemer/rare_mistral/scripts/')
+    # os.chdir('/work/um0878/user_data/froemer/rare_mistral/scripts/')
 
-    domain = sys.argv[1]
-    year = sys.argv[2]
-    month = sys.argv[3]
+    year = sys.argv[1]
+    month = sys.argv[2]
+    
+    # year, month = '2011', '01'
 
-    fluxes, avg, std = orbits_to_monthly_mean(domain, year, month)
+    print(year, month)
+
+    domains = ['global/all-sky/land+ocean',
+              'global/all-sky/ocean-only',
+              'global/all-sky/land-only',
+              'global/clear-sky/land+ocean',
+              'global/clear-sky/ocean-only',
+              'global/clear-sky/land-only',
+              'global/cloudy/land+ocean',
+              'global/cloudy/ocean-only',
+              'global/cloudy/land-only',
+              'tropics/all-sky/land+ocean',
+              'tropics/all-sky/ocean-only',
+              'tropics/all-sky/land-only',
+              'tropics/clear-sky/land+ocean',
+              'tropics/clear-sky/ocean-only',
+              'tropics/clear-sky/land-only',
+              'tropics/cloudy/land+ocean',
+              'tropics/cloudy/ocean-only',
+              'tropics/cloudy/land-only',
+              'extra/all-sky/land+ocean',
+              'extra/all-sky/ocean-only',
+              'extra/all-sky/land-only',
+              'extra/clear-sky/land+ocean',
+              'extra/clear-sky/ocean-only',
+              'extra/clear-sky/land-only',
+              'extra/cloudy/land+ocean',
+              'extra/cloudy/ocean-only',
+              'extra/cloudy/land-only']
+#
+#    np.save('/work/um0878/user_data/froemer/rare_mistral/data/IASI/test_new/'
+#            'test/domains.npy', domains)
+#
+#    months = []
+#    for year in [2011, 2013, 2016, 2017]:
+#        for month in ['01', '03', '05', '07', '09', '11']:
+#            months.append(f'{year}/{month}')
+#    np.save('/work/um0878/user_data/froemer/rare_mistral/data/IASI/test_new/'
+#            'test/months.npy', months)
+
+    for d, domain in enumerate(domains):
+        print(domain)
+        flux, avg, std, area = orbits_to_monthly_mean(
+            domain, year, month)
 
     end = time.process_time()
     print(end - start)
+    
+    # %% inferring orbit wise (option 1) does deliver worse results if number 
+    # of orbits is different between domains: when averaging, the wrong weights
+    # are assigned from the first time one orbit is nan onwards
+    # mean value is better than that, but still worse compared to average with
+    # correctly assinged weights
+    # solution: calculate monthly average separately for every domains with 
+    # correct weights, then infer from those monthly means using monthly total
+    # areas
+    
+    # option 1
+    # length = np.min(np.array((len(fluxes[0]), len(fluxes[1]), len(fluxes[2]))))
+    # extra = (fluxes[0][:length]*areas[0][:length].reshape(areas[0][:length].shape[0], 1) - 
+    #           fluxes[1][:length]*areas[1][:length].reshape(areas[1][:length].shape[0], 1))/ \
+    #         areas[2][:length].reshape(areas[2][:length].shape[0], 1)
+            
+    # extra_area = areas[0] - areas[1]
+
+    # option 2
+    # domains = ['global/clear-sky/land+ocean',
+    #        'tropics/clear-sky/land+ocean',
+    #        'extra/clear-sky/land+ocean']
+
+    # fluxes = []
+    # areas = []
+    # average = np.zeros((3, 8461))
+    # total_area = np.zeros((3))
+    # for d, domain in enumerate(domains):
+    #     print(domain)
+    #     flux, avg, std, area = orbits_to_monthly_mean(
+    #         domain, year, month)
+    #     fluxes.append(flux)
+    #     areas.append(area)
+    #     average[d, :] = np.average(flux.T, weights=area.flatten(), axis=1)
+    #     total_area[d] = np.sum(area)
+        
+    # avg_est = (average[0]*total_area[0] - average[1]*total_area[1]) / total_area[2]
+
+    # path = '/work/um0878/user_data/froemer/rare_mistral/data/IASI/test_new/'\
+    #        'test/{}/{}/{}/'.format(domain, year, month)
+
+    # np.save(f'{path}/inferred_avg_flux.npy', avg_est, allow_pickle=False,
+    #         fix_imports=False)
+    # np.save(f'{path}/inferred_total_area.npy', total_area[2],
+    #         allow_pickle=False, fix_imports=False)
