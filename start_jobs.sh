70cf92676f5f1881e0d75ef43b857c8e8711ce84
roemulus <roemerflo@gmail.com>
2021-11-30 11:41:04 +0100

    incorporated changes from test script: longer time limit, different path and pathname

--- start_jobs.sh
+++ start_jobs.sh
@@ -2,19 +2,16 @@
 year=$1
 month=$2
 
-scripts='/work/um0878/user_data/froemer/rare_mistral/scripts/eumetsat'
+scripts='/pf/u/u301023/iasi'
 
-for year in {2007..2021}
-do
-mkdir -p $data/$year
-done
+day=1
 
-for day in {1..9}
-do
-sbatch_simple_bigmem compute2 IASI$year$month'0'$day 36 00:30:00 python $filepath'process_iasi.py' $year $month '0'$day
-done
+#for day in {1..9}
+#do
+sbatch_simple_bigmem compute2 IASI$year$month'0'$day 36 01:30:00 python $scripts/process_iasi.py $year $month '0'$day
+#done
 
-for day in {10..31}
-do
-sbatch_simple_bigmem compute2 IASI$year$month$day 36 00:30:00 python $filepath'process_iasi.py' $year $month $day
-done
+#for day in {10..31}
+#do
+#sbatch_simple_bigmem compute2 IASI$year$month$day 36 01:30:00 python $scripts/process_iasi.py $year $month $day
+#done
