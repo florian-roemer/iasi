38787d39a6e706285b1abec00e06f35d951b03be
roemulus <roemerflo@gmail.com>
2021-11-30 11:47:52 +0100

    changed time limit

--- start_jobs.sh
+++ start_jobs.sh
@@ -6,10 +6,10 @@ scripts='/DSNNAS/Repro_Temp/users/vijuj/git/iasi'
 
 for day in {1..9}
 do
-sbatch_simple_bigmem compute2 IASI$year$month'0'$day 36 00:30:00 python $scripts/'process_iasi.py' $year $month '0'$day
+sbatch_simple_bigmem compute2 IASI$year$month'0'$day 36 01:30:00 python $scripts/process_iasi.py $year $month '0'$day
 done
 
 for day in {10..31}
 do
-sbatch_simple_bigmem compute2 IASI$year$month$day 36 00:30:00 python $scripts/'process_iasi.py' $year $month $day
+sbatch_simple_bigmem compute2 IASI$year$month$day 36 01:30:00 python $scripts/process_iasi.py $year $month $day
 done
